03727637c47d762b887316cbb7caa2af51572fae
siredtang <siredtang@gmail.com>
2026-07-24 21:30:36 -0400

    Update urls.py

--- config/config/urls.py
+++ config/config/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
 
     path("", include(("studybuddy_cms.urls", "studybuddy_cms"), namespace="studybuddy_cms")),
+    # quiz APIs (generate, history, stats, recommendations)
+    path("", include(("studybuddy_quiz.urls", "studybuddy_quiz"), namespace="studybuddy_quiz")),
 ]
